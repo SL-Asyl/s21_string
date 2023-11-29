f7bf7aa885a3cec7b922e853fd9b87534b127e81
SL-Asyl <148560817+SL-Asyl@users.noreply.github.com>
2023-11-29 11:37:45 +0300

    Create memset.c

---
+++ memset.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *s21_memset(void *str, int c, size_t n);
+void print_array(const void *arr, size_t n);
+
+int main()
+{
+    int arr[10];
+
+    s21_memset(arr, 0, sizeof(arr));
+
+    print_array(arr, sizeof(arr));
+
+    return 0;
+}
+
+void *s21_memset(void *str, int c, size_t n)
+{
+    for (int x = 0; x < n; x += 1) {
+        ((char *)str)[x] = c;
+    }
+    return str;
+}
+
+void print_array(const void *arr, size_t n)
+{
+    const char *arr_ptr = arr;
+
+    for (size_t i = 0; i < n; i++) {
+        printf("%d ", arr_ptr[i]);
+    }
+    printf("\n");
+}
